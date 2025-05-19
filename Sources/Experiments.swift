6bc2fde83dd98c38e3c31f4268785e5fbaf7df37
Chinosu <46995931+Chinosu@users.noreply.github.com>
2025-05-19 14:34:51 +0900

    exper: detect app activate with AX

--- Sources/Experiments.swift
+++ Sources/Experiments.swift
@@ -3,7 +3,6 @@ import AppKit
 func experiments() {
     guard let app = NSWorkspace.shared.frontmostApplication else {
         print("frontmost app was null, bye!")
-
         return
     }
 
@@ -30,24 +29,36 @@ func experiments() {
     print(mainWin)
     print(focusedWin)
 
-    let observer = {
+    let winObs = {
         var observer: AXObserver?
         AXObserverCreate(
             app.processIdentifier,
-            { obs, elem, notif, userData in print("winch") },
+            { obs, elem, notif, userData in print("win ch") },
+            &observer
+        )
+        return observer!
+    }()
+    let appObs = {
+        var observer: AXObserver?
+        AXObserverCreate(
+            app.processIdentifier,
+            { obs, elem, notif, userData in print("app ch (ax) // only observes one app") },
             &observer
         )
         return observer!
     }()
 
     AXObserverAddNotification(
-        observer, axApp, kAXFocusedWindowChangedNotification as CFString, nil)
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(observer), .defaultMode)
+        winObs, axApp, kAXFocusedWindowChangedNotification as CFString, nil)
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(winObs), .defaultMode)
+    AXObserverAddNotification(
+        appObs, axApp, kAXApplicationActivatedNotification as CFString, nil)
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(appObs), .defaultMode)
 
     NSWorkspace.shared.notificationCenter.addObserver(
         forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: .main
     ) { notif in
-        print("appch")
+        print("app ch (ns)")
     }
 
     RunLoop.current.run()
