58a90c0cf2583a7b804e8220aac344285ee63fa1
Chinosu <46995931+Chinosu@users.noreply.github.com>
2025-05-20 01:19:37 +0900

    exper: remove old

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,13 +3,10 @@ import Foundation
 let argv = CommandLine.arguments
 if argv.count > 1 {
     print("extra arguments detected-\u{10}doing experiments only")
-    // Experiments.observers()
-    // Experiments.topWindow()
     Task {
         _ = await Observers.init()
         print("init finished. i'm waiting!")
     }
-
     RunLoop.current.run()
     exit(0)
 }
