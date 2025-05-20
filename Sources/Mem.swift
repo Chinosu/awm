eab770c624c7aa3cf5b53107d25a977571b410dc
Chinosu <46995931+Chinosu@users.noreply.github.com>
2025-05-20 18:59:08 +0900

    exper: custom operators

---
+++ Sources/Mem.swift
@@ -0,0 +1,38 @@
+postfix operator &
+postfix func & <T>(left: inout T) -> UnsafeMutablePointer<T> {
+    withUnsafeMutablePointer(to: &left) { $0 }
+}
+
+postfix operator *
+postfix func * <T>(left: UnsafeMutablePointer<T>) -> T {
+    left.pointee
+}
+
+infix operator =*
+func =* <T>(left: UnsafeMutablePointer<T>, right: T) {
+    left.pointee = right
+}
+
+extension UnsafeMutablePointer {
+    var a: Int {
+        return 2
+    }
+}
+
+postfix operator &~
+postfix func &~ <T>(left: inout T) -> UnsafeMutableRawPointer {
+    UnsafeMutableRawPointer(left&)
+}
+
+infix operator => : CastingPrecedence
+func => <T>(left: UnsafeMutableRawPointer, right: T.Type) -> UnsafeMutablePointer<T> {
+    left.assumingMemoryBound(to: right)
+}
+
+func dev() {
+    var x = 42
+    let ptr = x&
+    print(ptr*)
+    ptr =* 2
+    // print(ptr.*)  // 42
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -3,11 +3,14 @@ import Foundation
 let argv = CommandLine.arguments
 if argv.count > 1 {
     print("extra arguments detected-\u{10}doing experiments only")
-    Task {
-        _ = await Observers.init()
-        print("init finished. i'm waiting!")
-    }
-    RunLoop.current.run()
+    // Task {
+    //     _ = await Observers.init()
+    //     print("init finished. i'm waiting!")
+    // }
+    // RunLoop.current.run()
+    // print(++"asd")
+
+    dev()
     exit(0)
 }
 
