6462176e0057995d838e3cdee1cf9baaa90516c9
Chinosu <46995931+Chinosu@users.noreply.github.com>
2025-05-23 12:39:36 +0900

    `cmd+shift+tab` also walks

--- Sources/Hotkeys.swift
+++ Sources/Hotkeys.swift
@@ -30,75 +30,56 @@ private func keyDispatch(
 ) -> Unmanaged<CGEvent>? {
     guard
         event.flags.contains(.maskNonCoalesced)
-            && event.flags.isDisjoint(with: [.maskShift, .maskControl, .maskHelp, .maskSecondaryFn])
+            && event.flags.isDisjoint(with: [.maskControl, .maskHelp, .maskSecondaryFn])
     else { return Unmanaged.passUnretained(event) }
 
     let alt = event.flags.contains(.maskAlternate)
     let cmd = event.flags.contains(.maskCommand)
+    let shift = event.flags.contains(.maskShift)
     let key = Int(event.getIntegerValueField(.keyboardEventKeycode))
     let autorepeat = 0 != event.getIntegerValueField(.keyboardEventAutorepeat)
 
     // print("=> \(key) \(autorepeat) \(kind) \(alt)+\(cmd)")
 
     let wc = Unmanaged<WindowConductor>.fromOpaque(userInfo!).takeUnretainedValue()
-    switch (kind, key, autorepeat, (alt, cmd)) {
-    case (.keyDown, kVK_ANSI_1, false, (false, true)):
-        Task { await wc.doRaise(index: 0) }
-    case (.keyDown, kVK_ANSI_1, true, (false, true)):
-        break
+    switch (kind, key, autorepeat, (alt, cmd, shift)) {
+    case (.keyDown, kVK_ANSI_1, false, (false, true, false)): Task { await wc.doRaise(index: 0) }
+    case (.keyDown, kVK_ANSI_1, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_2, false, (false, true)):
-        Task { await wc.doRaise(index: 1) }
-    case (.keyDown, kVK_ANSI_2, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_2, false, (false, true, false)): Task { await wc.doRaise(index: 1) }
+    case (.keyDown, kVK_ANSI_2, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_3, false, (false, true)):
-        Task { await wc.doRaise(index: 2) }
-    case (.keyDown, kVK_ANSI_3, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_3, false, (false, true, false)): Task { await wc.doRaise(index: 2) }
+    case (.keyDown, kVK_ANSI_3, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_4, false, (false, true)):
-        Task { await wc.doRaise(index: 3) }
-    case (.keyDown, kVK_ANSI_4, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_4, false, (false, true, false)): Task { await wc.doRaise(index: 3) }
+    case (.keyDown, kVK_ANSI_4, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_5, false, (false, true)):
-        Task { await wc.doRaise(index: 4) }
-    case (.keyDown, kVK_ANSI_5, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_5, false, (false, true, false)): Task { await wc.doRaise(index: 4) }
+    case (.keyDown, kVK_ANSI_5, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_6, false, (false, true)):
-        Task { await wc.doRaise(index: 5) }
-    case (.keyDown, kVK_ANSI_6, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_6, false, (false, true, false)): Task { await wc.doRaise(index: 5) }
+    case (.keyDown, kVK_ANSI_6, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_7, false, (false, true)):
-        Task { await wc.doRaise(index: 6) }
-    case (.keyDown, kVK_ANSI_7, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_7, false, (false, true, false)): Task { await wc.doRaise(index: 6) }
+    case (.keyDown, kVK_ANSI_7, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_8, false, (false, true)):
-        Task { await wc.doRaise(index: 7) }
-    case (.keyDown, kVK_ANSI_8, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_8, false, (false, true, false)): Task { await wc.doRaise(index: 7) }
+    case (.keyDown, kVK_ANSI_8, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_9, false, (false, true)):
-        Task { await wc.doRaise(index: 8) }
-    case (.keyDown, kVK_ANSI_9, true, (false, true)):
-        break
+    case (.keyDown, kVK_ANSI_9, false, (false, true, false)): Task { await wc.doRaise(index: 8) }
+    case (.keyDown, kVK_ANSI_9, true, (false, true, false)): break
 
-    case (.keyDown, kVK_Tab, false, (false, true)):
-        Task { await wc.doRaisePrev() }
-    case (.keyDown, kVK_Tab, true, (false, true)):
-        break
+    case (.keyDown, kVK_Tab, false, (false, true, false)): Task { await wc.doRaisePrev() }
+    case (.keyDown, kVK_Tab, true, (false, true, false)): break
 
-    case (.keyDown, kVK_ANSI_Grave, false, (false, true)):
-        Task { await wc.doRaiseWalk() }
-    case (.keyDown, kVK_ANSI_Grave, true, (false, true)):
-        break
+    case (.keyDown, kVK_Tab, false, (false, true, true)): Task { await wc.doRaiseWalk() }
+    case (.keyDown, kVK_Tab, true, (false, true, true)): break
 
-    default:
-        return Unmanaged.passUnretained(event)
+    case (.keyDown, kVK_ANSI_Grave, false, (false, true, false)): Task { await wc.doRaiseWalk() }
+    case (.keyDown, kVK_ANSI_Grave, true, (false, true, false)): break
+
+    default: return Unmanaged.passUnretained(event)
     }
 
     return nil
