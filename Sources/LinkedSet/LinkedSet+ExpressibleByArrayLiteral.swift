da402b83b04529fb6c83732378f29fd8d1876f95
Chinosu <46995931+Chinosu@users.noreply.github.com>
2025-05-27 22:06:13 +0900

    renamee

--- Sources/LinkedSet/LinkedSet+ExpressibleByArrayLiteral.swift
+++ Sources/LinkedSet/LinkedSet+ExpressibleByArrayLiteral.swift
@@ -2,7 +2,6 @@ extension LinkedSet: ExpressibleByArrayLiteral {
     typealias ArrayLiteralElement = Element
 
     init(arrayLiteral elements: Element...) {
-        // assume: deleteExisting = true
         for elem in elements { self.reappend(elem) }
     }
 }
